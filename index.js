f7192b8312f2a5db395a6ff4f7f4b54d0b4c3a0d
unknown <>
2021-02-17 22:45:31 +0900

    graph

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const mcdata = require("mcdata");
 
 const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
 
+const cron = require("node-cron");
+
 function saveData() {
     api.updateBin({
         id: "602520643b303d3d964f1ffd",
@@ -26,8 +28,10 @@ function saveData() {
     });
 }
 let kokocityChannel;
+let dvxChannel;
 client.once("ready", () => {
     kokocityChannel = client.channels.cache.get("810173363485933568");
+    dvxChannel = client.channels.cache.get("811508382254628894");
 });
 
 client.on("message", (message2) => {
@@ -132,6 +136,7 @@ client.on("message", (message2) => {
             }
             saveData();
         } else if (message2.channel.id == "810046656170688522") {
+            console.log(message);
             switch (true) {
                 case /^도움말$/.test(message):
                     message2.reply(`
@@ -199,31 +204,56 @@ DCB 잔액 : ${data.bank.account[msg[1].slice(3, 21)]}Đ
             }
         } else if (message2.channel.id == "810173363485933568") {
             switch (true) {
-                case /^매도 [1-9][0-9]* [1-9][0-9]*$/.test(message):
-                    if (data.stock.kokocity.deal[msg[1]]) {
-                        data.stock.kokocity.deal[msg[1]].sell.push([message2.author.id, Number(msg[2])]);
-                        data.stock.kokocity.deal[msg[1]].sellTotal += Number(msg[2]);
+                case /^매도 [1-9][0-9]{3,} [1-9][0-9]*$/.test(message):
+                    var time = new Date();
+                    var timeString = `${time.getHours().toString().padStart(2, "0")}${time.getMinutes().toString().padStart(2, "0")}${time.getSeconds().toString().padStart(2, "0")}`;
+                    if ((time.getHours() == 23 && time.getMinutes() >= 55) || (time.getHours() == 0 && time.getMinutes() <= 5)) {
+                        dvxChannel.send(`<@${message2.author.id}>, 주식 거래 불가능 시간대입니다.`);
+                    } else if (Number(msg[2]) > data.stock.kokocity.stocks[message2.author.id]) {
+                        dvxChannel.send(`<@${message2.author.id}>, 보유하고 있는 주식이 부족합니다.`);
                     } else {
-                        data.stock.kokocity.deal[msg[1]] = {sell: [[message2.author.id, Number(msg[2])]], buy: [], sellTotal: Number(msg[2]), buyTotal: 0};
+                        dvxChannel.send(`<@${message2.author.id}>, 매도 주문이 접수되었습니다.`);
+                        data.stock.kokocity.stocks[message2.author.id] -= Number(msg[2]);
+                        if (data.stock.kokocity.deal[msg[1]]) {
+                            data.stock.kokocity.deal[msg[1]].sell.push([message2.author.id, Number(msg[2]), timeString]);
+                            data.stock.kokocity.deal[msg[1]].sellTotal += Number(msg[2]);
+                        } else {
+                            data.stock.kokocity.deal[msg[1]] = {sell: [[message2.author.id, Number(msg[2]), timeString]], buy: [], sellTotal: Number(msg[2]), buyTotal: 0};
+                        }
+                        kokocityCharts();
                     }
-                    dom();
+                    saveData();
                     break;
-                case /^매수 [1-9][0-9]* [1-9][0-9]*$/.test(message):
-                    if (data.stock.kokocity.deal[msg[1]]) {
-                        data.stock.kokocity.deal[msg[1]].buy.push([message2.author.id, Number(msg[2])]);
-                        data.stock.kokocity.deal[msg[1]].buyTotal += Number(msg[2]);
+                case /^매수 [1-9][0-9]{3,} [1-9][0-9]*$/.test(message):
+                    var time = new Date();
+                    var timeString = `${time.getHours().toString().padStart(2, "0")}${time.getMinutes().toString().padStart(2, "0")}${time.getSeconds().toString().padStart(2, "0")}`;
+                    if ((time.getHours() == 23 && time.getMinutes() >= 55) || (time.getHours() == 0 && time.getMinutes() <= 5)) {
+                        dvxChannel.send(`<@${message2.author.id}>, 주식 거래 불가능 시간대입니다.`);
+                    } else if (Number(msg[2]) * Number(msg[1]) > data.bank.account[message2.author.id]) {
+                        dvxChannel.send(`<@${message2.author.id}>, 잔액이 부족합니다.`);
                     } else {
-                        data.stock.kokocity.deal[msg[1]] = {sell: [], buy: [[message2.author.id, Number(msg[2])]], sellTotal: 0, buyTotal: Number(msg[2])};
+                        dvxChannel.send(`<@${message2.author.id}>, 매수 주문이 접수되었습니다.`);
+                        data.bank.account[message2.author.id] -= Number(msg[2]) * Number(msg[1]);
+                        if (data.stock.kokocity.deal[msg[1]]) {
+                            data.stock.kokocity.deal[msg[1]].buy.push([message2.author.id, Number(msg[2]), timeString]);
+                            data.stock.kokocity.deal[msg[1]].buyTotal += Number(msg[2]);
+                        } else {
+                            data.stock.kokocity.deal[msg[1]] = {sell: [], buy: [[message2.author.id, Number(msg[2]), timeString]], sellTotal: 0, buyTotal: Number(msg[2])};
+                        }
+                        kokocityCharts();
                     }
-                    dom();
+                    break;
+                    saveData();
+                default:
+                    message2.delete();
                     break;
             }
         }
     }
 });
 
-async function dom() {
-    var canvasRenderService = new ChartJSNodeCanvas({width: 200, height: 450});
+async function kokocityCharts() {
+    var canvasRenderService = new ChartJSNodeCanvas({width: 850, height: 400});
     var prices = Object.keys(data.stock.kokocity.deal).sort((a, b) => Number(a) > Number(b) ? 1 : -1);
     var sell = prices.map((a) => -data.stock.kokocity.deal[a].sellTotal);
     var buy = prices.map((a) => data.stock.kokocity.deal[a].buyTotal);
@@ -265,12 +295,229 @@ async function dom() {
         }
     });
     var date = new Date();
-    var id = `${date.getFullYear()}${date.getMonth()}${date.getDay()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+    var id = `${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
     var file = new Discord.MessageAttachment(image, `dom-${id}.png`);
     var embed = new Discord.MessageEmbed().setTitle("호가창").attachFiles([file]).setImage(`attachment://dom-${id}.png`);
     var fetched = await kokocityChannel.messages.fetch({limit: 2});
     kokocityChannel.bulkDelete(fetched);
     kokocityChannel.send({embed});
 }
+
+cron.schedule("0 0 * * *", () => {
+    var open = 0;
+    var high = 0;
+    var low = Infinity;
+    var close = 0;
+    var openTime = "999999";
+    var closeTime = "000000";
+    var prices = Object.keys(data.stock.kokocity.deal).sort((a, b) => Number(a) > Number(b) ? 1 : -1);
+    var sellOffers = [];
+    var buyOffers = [];
+    prices.map((price) => {
+        sellOffers += data.stock.kokocity.deal[price].sell.map((offer) => offer + [Number(price)]);
+        buyOffers += data.stock.kokocity.deal[price].buy.map((offer) => offer + [Number(price)]);
+    });
+    var notice = "";
+    var volume = 0;
+    for (var i in sellOffers) {
+        for (var j in buyOffers) {
+            if (buyOffers[j][1] != 0 && buyOffers[j][3] >= sellOffers[i][3]) {
+                if (buyOffers[j][1] <= sellOffers[i][1]) {
+                    sellOffers[i][1] -= buyOffers[j][1];
+                    buyOffers[j][1] = 0;
+                    volume += buyOffers[j][1];
+                    notice += `<@${sellOffers[i][0]}> -> <@${buyOffers[j][0]}> 주당 ${sellOffers[i][3].toLocaleString()}Đ에 ${buyOffers[j][1]}주가 거래 체결되었습니다.\n`;
+                    data.stock.kokocity.stocks[buyOffers[j][0]] += buyOffers[j][1];
+                    data.bank.account[sellOffers[i][0]] += sellOffers[i][3] * buyOffers[j][1];
+                    data.bank.account[buyOffers[j][0]] += buyOffers[j][1] * (buyOffers[j][3] - sellOffers[i][3]);
+                    if (sellOffers[i][2] < openTime) {
+                        openTime = sellOffers[i][2];
+                        open = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][2] >= closeTime) {
+                        closeTime = sellOffers[i][2];
+                        close = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][3] < low) {
+                        low = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][3] > high) {
+                        high = sellOffers[i][3];
+                    }
+                    if (buyOffers[j][1] == sellOffers[i][1]) {
+                        break;
+                    }
+                } else if (buyOffers[j][3] > sellOffers[i][3]) {
+                    buyOffers[j][1] -= sellOffers[i][1];
+                    sellOffers[i][1] = 0;
+                    volume += sellOffers[i][1];
+                    notice += `<@${sellOffers[i][0]}> -> <@${buyOffers[j][0]}> 주당 ${sellOffers[i][3].toLocaleString()}Đ에 ${sellOffers[i][1]}주가 거래 체결되었습니다.\n`;
+                    data.stock.kokocity.stocks[buyOffers[j][0]] += sellOffers[i][1];
+                    data.bank.account[sellOffers[i][0]] += sellOffers[i][3] * sellOffers[i][1];
+                    data.bank.account[buyOffers[j][0]] += sellOffers[j][1] * (buyOffers[j][3] - sellOffers[j][3]);
+                    if (sellOffers[i][2] < openTime) {
+                        openTime = sellOffers[i][2];
+                        open = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][2] >= closeTime) {
+                        closeTime = sellOffers[i][2];
+                        close = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][3] < low) {
+                        low = sellOffers[i][3];
+                    }
+                    if (sellOffers[i][3] > high) {
+                        high = sellOffers[i][3];
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    data.stock.kokocity.deal = {"a": 0};
+    for (var i of sellOffers) {
+        if (i[1] != 0) {
+            if (data.stock.kokocity.deal[i[3]]) {
+                data.stock.kokocity.deal[i[3]].sell.push(i.slice(0, 3));
+                data.stock.kokocity.deal[i[3]].sellTotal += i[1];
+            } else {
+                data.stock.kokocity.deal[i[3]] = {
+                    sell: [i.slice(0, 3)],
+                    buy: [],
+                    sellTotal: i[1],
+                    buyTotal: 0
+                };
+            }
+        }
+    }
+    for (var i of buyOffers) {
+        if (i[1] != 0) {
+            if (data.stock.kokocity.deal[i[3]]) {
+                data.stock.kokocity.deal[i[3]].buy.push(i.slice(0, 3));
+                data.stock.kokocity.deal[i[3]].buyTotal += i[1];
+            } else {
+                data.stock.kokocity.deal[i[3]] = {
+                    sell: [],
+                    buy: [i.slice(0, 3)],
+                    sellTotal: 0,
+                    buyTotal: i[1]
+                };
+            }
+        }
+    }
+    if (open != 0 || high != 0 || low != Infinity || close != 0) {
+        var time = new Date();
+        data.stock.kokocity.info.tohlc.push({
+            t: time.valueOf(),
+            o: open,
+            h: high,
+            l: low,
+            c: close
+        });
+        data.stock.kokocity.info.date.push(`${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`);
+        data.stock.kokocity.info.volume.push(volume);
+        (async () => {
+            var fetched = await kokocityChannel.messages.fetch({limit: 4});
+            kokocityChannel.bulkDelete(fetched);
+            var canvasRenderService = new ChartJSNodeCanvas({width: 850, height: 400});
+            var image = await canvasRenderService.renderToBuffer({
+                type: 'candlestick',
+                data: {
+                    datasets: [
+                        {
+                            label: "Daily Chart",
+                            data: data.stock.kokocity.info.tohlc
+                        }
+                    ]
+                }
+            });
+            var date = new Date();
+            var id = `${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+            var file = new Discord.MessageAttachment(image, `daily-${id}.png`);
+            var embed = new Discord.MessageEmbed().setTitle("일봉 차트").attachFiles([file]).setImage(`attachment://daily-${id}.png`);
+            kokocityChannel.send(embed);
+
+            canvasRenderService = new ChartJSNodeCanvas({width: 850, height: 400});
+            image = await canvasRenderService.renderToBuffer({
+                type: 'bar',
+                data: {
+                    labels: data.stock.kokocity.info.date,
+                    datasets: [
+                        {
+                            label: "VOLUME",
+                            backgroundColor: "#00FF00",
+                            data: sell
+                        }
+                    ]
+                }
+            });
+            date = new Date();
+            id = `${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+            file = new Discord.MessageAttachment(image, `volume-${id}.png`);
+            embed = new Discord.MessageEmbed().setTitle("거래량 차트").attachFiles([file]).setImage(`attachment://volume-${id}.png`);
+            kokocityChannel.send(embed);
+
+            kokocityChannel.send(
+`_증권 정보_
+**코코시티** ${close.toLocaleString()} _${data.stock.kokocity.info.tohlc[data.stock.kokocity.info.tohlc.length - 2].c <= close ? "▲" : "▼"} ${Math.abs(close - data.stock.kokocity.info.tohlc[data.stock.kokocity.info.tohlc.length - 2].c).toLocaleString()}_
+전일종가 : ${data.stock.kokocity.info.tohlc[data.stock.kokocity.info.tohlc.length - 2].c} 고가 : ${high.toLocaleString()} 저가 : ${low.toLocaleString()}
+거래량 : ${volume} 시가 총액 : ${(close * 41).toLocaleString()} DESPI : ${(close * 41 / 410 * 100).toLocaleString()}
+`
+            );
+
+            canvasRenderService = new ChartJSNodeCanvas({width: 850, height: 400});
+            var prices = Object.keys(data.stock.kokocity.deal).sort((a, b) => Number(a) > Number(b) ? 1 : -1);
+            var sell = prices.map((a) => -data.stock.kokocity.deal[a].sellTotal);
+            var buy = prices.map((a) => data.stock.kokocity.deal[a].buyTotal);
+            image = await canvasRenderService.renderToBuffer({
+                type: 'bar',
+                data: {
+                    labels: prices,
+                    datasets: [
+                        {
+                            label: "SELL",
+                            backgroundColor: "#0000FF",
+                            data: sell
+                        },
+                        {
+                            label: "BUY",
+                            backgroundColor: "#FF0000",
+                            data: buy
+                        }
+                    ]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: "PRICE"
+                    },
+                    scales: {
+                        xAxes: [{
+                            stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                            ticks: {
+                                callback: function (value, index, values) {
+                                    return value < 0 ? -value : value
+                                }
+                            }
+                        }]
+                    }
+                }
+            });
+            date = new Date();
+            id = `${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+            file = new Discord.MessageAttachment(image, `dom-${id}.png`);
+            embed = new Discord.MessageEmbed().setTitle("호가창").attachFiles([file]).setImage(`attachment://dom-${id}.png`);
+            kokocityChannel.send(embed);
+        })();
+    }
+    dvxChannel.send(notice);
+    saveData();
+}, {
+    scheduled: true,
+    timezone: "Asia/Seoul"
+}).start();
 
 client.login(process.env.BOT_TOKEN);
