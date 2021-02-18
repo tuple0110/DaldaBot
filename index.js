9fdbe3251b1239ff11427af1c5fa70a3edf8196b
unknown <>
2021-02-18 17:10:57 +0900

    candlestick

--- index.js
+++ index.js
@@ -315,7 +315,7 @@ async function kokocityCharts() {
     kokocityChannel.send({embed});
 }
 
-cron.schedule("8 17 * * *", () => {
+cron.schedule("15 17 * * *", () => {
     console.log("daily");
     var open = 0;
     var high = 0;
