13240548fc58a2c82bb2b1e3dacb961ffeb78973
unknown <>
2021-02-18 11:49:56 +0900

    chart

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ async function kokocityCharts() {
     kokocityChannel.send({embed});
 }
 
-cron.schedule("0 11 * * *", () => {
+cron.schedule("55 11 * * *", () => {
     console.log("daily");
     var open = 0;
     var high = 0;
